798b4b8f7ba1c40a222b52cf355c627f8579fa1d
Fernando Link <fernandolink@gmail.com>
2022-03-02 09:44:13 -0300

    06-operadores, incremento, comentário e variáveis.

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
   * atribuição (somador.php).
   * operações aritméticas (operadores.php).
   * funções aritméticas (funcoesaritmeticas.php).
+- aula06
+  * operadores (01-operadores.php).
+  * incremento e comentário (02-incremento.php).
+  * variáveis referenciadas (03-referencia.php).
+  * variáveis de variáveis (04-variaveisvariaveis.php)
---
+++ aula06/04-variaveisvariaveis.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title>Incremento</title>
+</head>
+<body>
+    <div>    
+        <?php
+            $x = "abc";
+            $$x = "def"; // variável de variável utiliza-se mais um $ na frente da variável
+            echo "O conteúdo da variável <strong>x</strong> é $x.";
+            echo "<br/>A variável <strong>$x</strong> criada recebeu o valor $abc.";
+        ?>
+    </div>
+</body>
+</html>
