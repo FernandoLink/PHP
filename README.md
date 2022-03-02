414460ac1ed51310bc272f4f0d424452ae287012
Fernando Link <fernandolink@gmail.com>
2022-03-01 22:33:32 -0300

    Aulas 4 e 5.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Estudos em PHP
 
+- aula04 - variáveis.
+- aula05 
+  * atribuição (somador.php).
+  * operações aritméticas (operadores.php).
+  * funções aritméticas (funcoesaritmeticas.php).
---
+++ aula04/tipos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title>Tipos de Variáveis</title>
+</head>
+<body>
+    <div>    
+        <?php
+            $nome = "Fernando Link";
+            $idade = 50;
+            echo "$nome tem $idade anos.";
+        ?>
+    </div>
+</body>
+</html>
